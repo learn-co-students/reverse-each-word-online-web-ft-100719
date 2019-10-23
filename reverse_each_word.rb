439ae61f9229aa0fcb8a45eb01bdb1c122fda4e2
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-23 02:02:23 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(sentence1)
-  return_array=[]
   array=sentence1.split (" ")
-  array.each do |word|
-    return_array<< word.reverse
-  end
-  return_array.join (" ")
+  array.collect do |word|
+  word.reverse
+  end.join (" ")
 end
